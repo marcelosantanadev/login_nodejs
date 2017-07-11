c69232697a7e5b9bfc27076c191d5e2254eb8276
vitor <vitor.juca@intelitechnology.com.br>
2017-07-11 16:05:48 -0400

    Removed bcrypt comments on login.js

--- controllers/login.js
+++ controllers/login.js
@@ -9,35 +9,7 @@ exports.getLoginPage = function (req, res) {
   };
 
   res.render('login', context);
-<<<<<<< HEAD
-};
-=======
 }
-
-// function gensalt() {
-//   var logRounds = 10;
-//   var BCRYPT_SALT_LEN = 16;
-//   var random = secureRandom();
-//   var rs = StringBuffer();
-//   var rnd = bytearray.writeByte(BCRYPT_SALT_LEN);
-//
-//   random.nextBytes(rnd);
-//
-//   rs.append("$2a$");
-//   if (logRounds < 10)
-//     rs.append("0");
-//   if (logRounds > 30) {
-//     throw Error("logRounds exceeds maximum (30)")
-//   }
-//
-//   rs.append(logRounds.toString());
-//   rs.append("$");
-//   rs.append(encodeBase64(rnd, rnd.size));
-//
-//   return rs.toString();
-// }
-⁠⁠⁠⁠
->>>>>>> eaea72a789194957a040f82138f73d26b7490fcd
 
 exports.postLoginPage = function (req, res) {
   var body = req.body;
